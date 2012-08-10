7bbcb6b115f086591f96a2f84ac6f4c695ad637d
gmarballi <gaurav_marballi@mcgraw-hill.com>
2012-08-10 14:42:46 -0400

    added verse extraction and refactoring

--- XMLReader/src/ReadXMLFile.java
+++ XMLReader/src/ReadXMLFile.java
@@ -14,26 +14,139 @@ class Shloka {
 	public String sanskrit_verse ;
 	public String english_verse ;
 	public String commentary;
+	String content ;
 	
 	Shloka(String thetitle, String thecontent) {
 		title = thetitle ;
 		sanskrit_verse = "" ;
 		english_verse = "" ;
 		commentary = thecontent ;
+		content = thecontent ;
+		
+		extractSanskritVerse() ;
+		extractEnglishVerse() ;
+	}
+	
+	void extractSanskritVerse() {
+		Pattern p = Pattern.compile("<i>(.*[|][|][ ]*[0-9]+[ ]*[|][|])") ;
+		Matcher m = p.matcher(content) ;
+		
+		String htmlVerse = "" ;
+		
+		if (m.find()) {
+			htmlVerse = m.group(1) ;
+			//System.out.println(">>" + m.group(1) + "<<") ;
+			//System.out.println(m.start() + " " + m.end() + " " + m.group());
+		}
+		
+		htmlVerse = htmlVerse.replaceAll("<i>", "") ;
+		htmlVerse = htmlVerse.replaceAll("</i>", "") ;
+		htmlVerse = htmlVerse.replaceAll("<br[ ]*/>", " ") ;
+		
+		System.out.println(htmlVerse) ;
+		
+		sanskrit_verse = htmlVerse ;
+	}
+	
+	void extractEnglishVerse() {
+		Pattern p = Pattern.compile("<b>(.*)</b>") ;
+		Matcher m = p.matcher(content) ;
+		
+		String htmlVerse = "" ;
+		
+		if (m.find()) {
+			htmlVerse = m.group(1) ;
+			//System.out.println(">>" + m.group(1) + "<<") ;
+			//System.out.println(m.start() + " " + m.end() + " " + m.group());
+		}
+		
+		htmlVerse = htmlVerse.replaceAll("<b>", "") ;
+		htmlVerse = htmlVerse.replaceAll("</b>", "") ;
+				htmlVerse = htmlVerse.replaceAll("<br[ ]*/>", " ") ;
+		
+		System.out.println(htmlVerse) ;
+		
+		english_verse = htmlVerse ;
 	}
 }
 
-public class ReadXMLFile {
+class XMLParser {
 	
-	public static List<Shloka> shlokas = new Vector<Shloka>() ;
+	static List<Shloka> shlokas = new Vector<Shloka>() ;
+	
+	static String getTagValue(String sTag, Element eElement) {
+		NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+	 
+	        Node nValue = (Node) nlList.item(0);
+	 
+		return nValue.getNodeValue();
+	  }
+	  
+	  static void writeFile(String path) throws Exception {
+		  BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
+		  
+		  for (int i = 0; i < shlokas.size(); ++i) {
+			  writer.write(shlokas.get(i).title) ;
+			  writer.write(shlokas.get(i).commentary) ;
+		  }
+		  
+		  writer.close() ;
+	  }
+
+	
+	static List<Shloka> doParse(String inFileName, String outFileName) throws Exception {
+		  
+		 	File fXmlFile = new File("c:\\users\\gmarballi\\downloads\\blog-07-25-2012.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+	 
+			//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+			NodeList nList = doc.getElementsByTagName("entry");
+			//System.out.println("-----------------------");
+	 
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+	 
+			   Node nNode = nList.item(temp);
+			   if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+	 
+			      Element eElement = (Element) nNode;
+			      
+			      String title = getTagValue("title", eElement);
+			      
+			      if (title.contains("hapter 2")) {
+			    	  
+			    	  String content = getTagValue("content", eElement);
+			    	  //String transformed_content = transformContent(content) ;
+			    	  //String transformed_title = transformTitle(title) ;
+			    	  System.out.println("Title : " + getTagValue("title", eElement));
+			    	  System.out.println("Content : " + content);
+		              //System.out.println("Nick Name : " + getTagValue("nickname", eElement));
+			      //System.out.println("Salary : " + getTagValue("salary", eElement));
+			    	  
+			    	  shlokas.add(new Shloka(title, content)) ;
+			      }
+	 
+			   }
+			}
+			
+			writeFile(outFileName) ;
+			
+			return shlokas ;
+	}
+		 	
+}
+
+class LatexTransform {
+	
 	
 	static String longtxt = "" +
 			" foo : asdasdas " +
 			" asdfasf : sdwrwefgvx " +
 			" bnmbnmnbmn : qwtyeryqtwerqyter " ;
 			
- 
-	public static void main(String argv[]) {
+	static void trialtransformer() {
 		
 		System.out.println(longtxt) ;
 		
@@ -49,53 +162,8 @@ public class ReadXMLFile {
 				//doMain() ;
 	}
 	
-	
-	static void doMain() {
- 
-	  try {
- 
-		File fXmlFile = new File("c:\\users\\gmarballi\\downloads\\blog-07-25-2012.xml");
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		Document doc = dBuilder.parse(fXmlFile);
-		doc.getDocumentElement().normalize();
- 
-		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-		NodeList nList = doc.getElementsByTagName("entry");
-		System.out.println("-----------------------");
- 
-		for (int temp = 0; temp < nList.getLength(); temp++) {
- 
-		   Node nNode = nList.item(temp);
-		   if (nNode.getNodeType() == Node.ELEMENT_NODE) {
- 
-		      Element eElement = (Element) nNode;
-		      
-		      String title = getTagValue("title", eElement);
-		      
-		      if (title.contains("hapter 2")) {
-		    	  
-		    	  String content = getTagValue("content", eElement);
-		    	  String transformed_content = transformContent(content) ;
-		    	  String transformed_title = transformTitle(title) ;
-		    	  System.out.println("Title : " + getTagValue("title", eElement));
-		    	  System.out.println("Content : " + transformed_content);
-	              //System.out.println("Nick Name : " + getTagValue("nickname", eElement));
-		      //System.out.println("Salary : " + getTagValue("salary", eElement));
-		    	  
-		    	  shlokas.add(new Shloka(transformed_title, transformed_content)) ;
-		      }
- 
-		   }
-		}
-		
-		writeFile("c:/users/gmarballi/downloads/gita-out.txt") ;
-	  } catch (Exception e) {
-		e.printStackTrace();
-	  }
-  }
- 
-	private static String transformContent(String str) {
+	 
+	static String html2latexContent(String str) {
 	
 		String result = str.replaceAll("[<]i[>]", "\\\\textit{") ;
 		result = result.replaceAll("[<]/i[>]", "}") ;
@@ -115,7 +183,7 @@ public class ReadXMLFile {
 		
 	}
 	
-	private static String transformTitle(String str) {
+	static String html2latexTitle(String str) {
 		
 		String result = "\\section{" + str + "}" +"\n"; 
 		return result ;
@@ -124,24 +192,24 @@ public class ReadXMLFile {
 	}
 	
 	
-  private static String getTagValue(String sTag, Element eElement) {
-	NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
+}
+
+public class ReadXMLFile {
+	
+
  
-        Node nValue = (Node) nlList.item(0);
- 
-	return nValue.getNodeValue();
-  }
-  
-  static void writeFile(String path) throws Exception {
-	  BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
-	  
-	  for (int i = 0; i < shlokas.size(); ++i) {
-		  writer.write(shlokas.get(i).title) ;
-		  writer.write(shlokas.get(i).commentary) ;
-	  }
-	  
-	  writer.close() ;
-  }
-  
- 
+	public static void main(String argv[]) {
+		
+		try {
+		//doMain() ;
+		XMLParser.doParse("c:\\users\\gmarballi\\downloads\\blog-07-25-2012.xml", "c:/users/gmarballi/downloads/gita-out.txt") ;
+		  
+		
+		} catch (Exception e) {
+				e.printStackTrace();
+			  }
+	}
+	
+
+
 }
